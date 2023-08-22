7c42a968958a1df93707a05c9114faff0fedde66
Cowboylaserkittenjetshark <82691052+Cowboylaserkittenjetshark@users.noreply.github.com>
2023-08-22 16:47:43 -0400

    Remove old comments

--- src/lib.rs
+++ src/lib.rs
@@ -16,8 +16,3 @@ pub fn run(config: Config) -> Result<(), Box<dyn std::error::Error>> {
     let v6 = config.fetchers.fetch_v6()?;
     config.cloudflare.update(v4, v6)
 }
-
-// TODO Put all functions directly in lib. Trying to abstract them out is a pain and now just overcomplicating things.
-// Construct the reqwest client, then pass it the functions.
-// fn get_zones(Client)
-// fn get_records(Client, Zone)
--- src/main.rs
+++ src/main.rs
@@ -13,50 +13,6 @@ fn main() {
         eprintln!("Problem updating records: {err}");
         process::exit(1);
     })
-    // let response = reqwest::blocking::Client::new()
-    //     .get("https://api.cloudflare.com/client/v4/zones")
-    //     .header(reqwest::header::CONTENT_TYPE, "application/json")
-    //     .header(
-    //         reqwest::header::AUTHORIZATION,
-    //         format!("Bearer {}", config.cloudflare.token),
-    //     )
-    //     .send()
-    //     .expect("Failure getting zone list")
-    //     .text()
-    //     .expect("Failed parsing body of zone list response");
-    // println!("{}", response);
-    // let parsed_response: ZoneListResp =
-    //     serde_json::from_str(&response).expect("Failed to parse zone list response");
-    // let zones_to_update = parsed_response.result.into_iter().filter(|zone| {
-    //     config
-    //         .cloudflare
-    //         .domains
-    //         .iter()
-    //         .any(|name| zone.name == *name)
-    // });
-    // for zone in zones_to_update {
-    //     let response = reqwest::blocking::Client::new()
-    //         .get(format!(
-    //             "https://api.cloudflare.com/client/v4/zones/{zone_id}/dns_records",
-    //             zone_id = zone.id
-    //         ))
-    //         .header(reqwest::header::CONTENT_TYPE, "application/json")
-    //         .header(
-    //             reqwest::header::AUTHORIZATION,
-    //             format!("Bearer {token}", token = config.cloudflare.token),
-    //         )
-    //         .send()
-    //         .expect("Failed getting dns record list for zone")
-    //         .text()
-    //         .expect("Failed parsing body of dns record list response");
-    //     let parsed_response: DnsListResp =
-    //         serde_json::from_str(&response).expect("Failed to parse dns record list response");
-    //     parsed_response
-    //         .result
-    //         .iter()
-    //         .filter(|rec| rec.record_type == "A")
-    //         .for_each(|rec| println!("{:?}", rec));
-    // }
 }
 
 fn get_config(config_path: &str) -> Result<Config, Box<dyn std::error::Error>> {
